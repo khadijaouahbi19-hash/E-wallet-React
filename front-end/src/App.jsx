8dff76821005559a31f746f87d75083cc809ced0
khadijaouahbi19-hash <khadijaouahbi19@gmail.com>
2026-04-28 00:37:27 +0100

    update avec useEffect

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-import Index from './components/index'
+import Index from "./components/index";
 import './App.css'
-import Login from './components/login'
-import Dashboard from './components/dashboard'
+import Login from "./components/login";
+import Dashboard from "./components/dashboard";
 import { finduserbymail } from "./data/database";
 
-function App() {
+
+function App(){
   const [showLogin,setShowLogin]=useState(false);
   const [user,setUser]=useState(finduserbymail("Ali@example.com","1232"));
   if(user) return <Dashboard user={user} setUser={setUser}/>;
-  if(showLogin) return <Login setShowLogin={setShowLogin} setUser={setUser}/>;
+  if(showLogin) return <Login user={user} setUser={setUser}/>;
   return <Index setShowLogin={setShowLogin}/>;
   
-
 }
-
 export default App
--- front-end/src/components/index.jsx
+++ front-end/src/components/index.jsx
@@ -1,9 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
-
-
 export default function Index({setShowLogin}) {
-
     return (
         <>
             <Header />
@@ -13,12 +10,12 @@ export default function Index({setShowLogin}) {
                         <h1>E-Wallet</h1>
                         <p>Gérez vos finances facilement et en toute sécurité. Simplifiez vos paiements et transactions grâce à notre solution moderne.</p>
                         <div className="buttons">
-                            <button className="btn btn-primary" id="Loginbtn" >Login</button>
+                            <button className="btn btn-primary" id="Loginbtn" onClick={() => setShowLogin(true)}  >Login</button>
                             <button className="btn btn-secondary" id="Signinbtn">Sign in</button>
                         </div>
                     </div>
                     <div className="hero-image">
-                        <img src="../src/assets/e-wallet6.gif" alt="E-Wallet Illustration" />
+                        <img src="../src/assets/e-Wallet6.gif" alt="E-Wallet Illustration" />
                     </div>
                 </section>
             </main>
